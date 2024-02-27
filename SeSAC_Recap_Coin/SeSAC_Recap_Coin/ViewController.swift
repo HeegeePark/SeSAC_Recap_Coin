8d4f5e356a6c8bdb6a1d6a9544125730f0905d7f
kizi <phg980513@naver.com>
2024-02-27 16:30:01 +0900

    [추가] 프로젝트 init
    - #1

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

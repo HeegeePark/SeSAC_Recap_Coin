36e2bc518f5953b413cb9cd3e107906372f67285
kizi <phg980513@naver.com>
2024-02-27 20:35:48 +0900

    [추가] #2 TrendingModel 구현

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/Model/TrendingModel.swift
@@ -0,0 +1,74 @@
+//
+//  TrendingModel.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/27/24.
+//
+
+import Foundation
+
+struct TrendingModel: Decodable {
+    let coins: [Coin]
+    let nfts: [NFT]
+}
+
+struct Coin: Decodable {
+    let item: CoinItem
+}
+
+struct CoinItem: Decodable {
+    let id: String  // 코인 이름
+    let name: String    // 코인 이름
+    let symbol: String  // 코인 통화 단위
+    let iconStr: String // 코인 아이콘 리소스
+    let data: CoinData
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case symbol
+        case iconStr = "small"
+        case data
+    }
+}
+
+struct CoinData: Decodable {
+    let price: String   // 코인 현재가
+    let priceChangePer24: PriceChangePer24  // 코인 변동폭
+    
+    enum CodingKeys: String, CodingKey {
+        case price
+        case priceChangePer24 = "price_change_percentage_24h"
+    }
+}
+
+// 변동폭
+struct PriceChangePer24: Decodable {
+    let krw: Double
+}
+
+struct NFT: Decodable {
+    let name: String    // NFT 토큰명
+    let symbol: String  // NFT 통화 단위
+    let iconStr: String // NFT 아이콘 리소스
+    let data: NFTData
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case symbol
+        case iconStr = "thumb"
+        case data
+    }
+}
+
+struct NFTData: Decodable {
+    let lowPrice: String   // NFT 최저가
+    let lowPriceChangePer24: String  // NFT 변동폭
+    
+    enum CodingKeys: String, CodingKey {
+        case lowPrice = "floor_price"
+        case lowPriceChangePer24 = "floor_price_in_usd_24h_percentage_change"
+    }
+}
+
+

116cb4bc3ce38ff1351d193a9f77476902ea88ec
kizi <phg980513@naver.com>
2024-02-28 14:48:58 +0900

    [추가] #3: BaseTableViewCell 구현

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/Base/BaseTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTableViewCell.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/28/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        contentView.backgroundColor = .clear
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+    }
+    
+    func configureLayout() {
+    }
+    
+    func configureView() {
+    }
+}

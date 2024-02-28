dca7d87a97dcff234ae6ac0ab52b50a6d6f5203a
kizi <phg980513@naver.com>
2024-02-28 20:28:45 +0900

    [추가] #3: FavoriteCoinsRepository 추가

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/Repository/FavoriteCoinsRepository.swift
@@ -0,0 +1,12 @@
+//
+//  FavoriteCoinsRepository.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/28/24.
+//
+
+import Foundation
+import RealmSwift
+
+final class FavoriteCoinsRepository: BaseRepository<FavoriteCoins> {
+}

c027d2c530b18d9c7311be07221a469e009d1760
kizi <phg980513@naver.com>
2024-02-28 15:25:20 +0900

    [추가] #3: UIFont 익스텐션 추가
    - 커스텀 폰트 정의

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/Extension/UIFont+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Extension.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/28/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title: UIFont = .systemFont(ofSize: 30, weight: .bold)
+    static let sfBold18: UIFont = .systemFont(ofSize: 18, weight: .bold)
+    static let sfRegular16: UIFont = .systemFont(ofSize: 16, weight: .regular)
+    static let sfRegular14: UIFont = .systemFont(ofSize: 14, weight: .regular)
+}

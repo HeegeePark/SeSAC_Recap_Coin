ae934d5b6e033821019531db1b060d285cf0b014
kizi <phg980513@naver.com>
2024-03-01 19:54:43 +0900

    [추가] #4: Double 익스텐션 추가

---
+++ SeSAC_Recap_Coin/SeSAC_Recap_Coin/Extension/Double+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Double+Extension.swift
+//  SeSAC_Recap_Coin
+//
+//  Created by 박희지 on 2/29/24.
+//
+
+import Foundation
+
+extension Double {
+    var preprocessPriceChangePer: String {
+        let sign = self > 0 ? "+": "-"
+        return sign + String(format: "%.2f", self) + "%"
+    }
+}
